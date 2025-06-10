5c07d7fe70f6d6adc2b12546090e472005ca79ab
unknown <dangabarin2020@gmail.com>
2025-06-10 01:28:25 +0100

    first commit

--- src/components/LandingTrusted.jsx
+++ src/components/LandingTrusted.jsx
@@ -6,10 +6,10 @@ function LandingTrusted({text,companies}) {
 
  
   return (
-    <div className='landingTrusted bg-white' style={{display:"block",padding:" 30px 0px",background:"transparent"}}>
+    <div className='landingTrusted bg-white' style={{display:"block",padding:" 30px 0px"}}>
        <section className='grid-img slider-containe' style={{display:"flex"}}>
      
-         <Marquee gradient={true} gradientWidth={140} >
+         <Marquee gradient={true} gradientWidth={130} >
         {companies.map(({img},index)=>{
           return (
       <img key={index} style={{height:"70px",padding:"0px 25px"}} className='normal-ig' src={img}/>
